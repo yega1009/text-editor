b8b0150ec388342210051a356d1034b5dbbf4190
Yega Cho <yega1009@gmail.com>
2023-11-22 19:44:38 -0500

    added comments to htmlRoutes.js

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,6 +1,12 @@
+// Import the path module to handle and transform file paths
 const path = require('path');
 
-module.exports = (app) =>
-  app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
-  );
+// Export a function that takes the app, Express instance, as an argument
+module.exports = (app) => {
+  // Define a GET route for the root of the application
+  app.get('/', (req, res) => {
+    // Send the index.html file as a response when the root route is requested
+    // Use path.join to create a path to the client's dist directory's index.html
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
+};
