3a02ec05d9a43dc2f4ca072b2789b8493cce81ea
Yega Cho <yega1009@gmail.com>
2023-11-23 00:04:31 -0500

    added StaleWhileRevalidate import from workbox-strategies

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,6 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
 const { CacheFirst } = require('workbox-strategies');
+const { StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
